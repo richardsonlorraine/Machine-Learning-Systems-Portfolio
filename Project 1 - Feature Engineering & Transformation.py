bbfd6b87a95852c3fdcd2cad02d59117b9928209
richardsonlorraine <richardsonlorraine@users.noreply.github.com>
2026-05-16 17:53:07 +0100

    Delete Project 1 - Feature Engineering & Transformation.py

--- Project 1 - Feature Engineering & Transformation.py
+++
@@ -1,3 +0,0 @@
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
-preprocessor = ColumnTransformer([("num", StandardScaler(), numerical_features), ("cat", OneHotEncoder(), categorical_features)])
